35dbb082edfac1a57dc594a452b5e2c415022968
KJTempas <fw6927mm@go.minneapolis.edu>
2020-10-09 11:00:17 -0500

    modify bitcoin to add functions

--- bitcoin.py
+++ bitcoin.py
@@ -5,30 +5,33 @@ coindesk_url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
 
 
 def main():
-    num = get_number_to_convert()
-    result = convert_bitcon_to_dollars(num)
-    display_result(result, num)
+    bitcoin = get_number_of_bitcoin()
+    dollars_exchange_rate = get_exchange_rate()
+    bitcoin_value_in_dollars = calculate_bitcoin_to_dollar(dollars_exchange_rate, bitcoin)
+    display_result(bitcoin, bitcoin_value_in_dollars)
 
-def get_number_to_convert():
-    num = float(input('Enter the number bitcoins you want to convert: '))
-    return num
+def get_number_of_bitcoin():
+    bitcoin = float(input('Enter the number bitcoins you want to convert: '))
+    return bitcoin
 
-def convert_bitcoin_to_dollars(num):
+def get_exchange_rate():
 
 #powered by Coindesk
-try:
-    response = requests.get(coindesk_url)
+    try:
+        response = requests.get(coindesk_url)
+        response.raise_for_status() #raises an exception for 400 or 500 status code
+        data = response.json() #convert response to json (like python dictionary)
+        dollars_exchange_rate = data['bpi']['USD']['rate_float']
+        return dollars_exchange_rate
+        #add validation
+    except Exception as e:
+        print(e)
+        print('There was an error making the request')
 
-    response.raise_for_status() #raises an exception for 400 or 500 status code
-    data = response.json() #convert response to json (like python dictionary)
-  
-    dollars_exchange_rate = data['bpi']['USD']['rate_float']
-    #print(dollars_exchange_rate)
-    #add validation
-    bitcoin =float(input('Enter the number of bitcoins: '))
+def calculate_bitcoin_to_dollar(dollars_exchange_rate, bitcoin):
     bitcoin_value_in_dollars= bitcoin * dollars_exchange_rate
-    print(f'$ {bitcoin_value_in_dollars} is the equivalent of {bitcoin} bitcoins')
-    #add formatting w/ decimals
-except Exception as e:
-    print(e)
-    print('There was an error making the request')
+    return bitcoin_value_in_dollars
+
+def display_result(bitcoin_value_in_dollars, bitcoin):
+    print(f'$ {bitcoin_value_in_dollars:.2f} is the equivalent of {bitcoin} bitcoins')
+        
